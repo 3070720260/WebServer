1e17f753c071b3b37c63bb4d4e3d318f60969218
3070720260 <60597605+3070720260@users.noreply.github.com>
2023-04-14 14:03:42 +0000

    aa

--- sql-father-frontend-public/src/.umi/exports.ts
+++ sql-father-frontend-public/src/.umi/exports.ts
@@ -5,7 +5,7 @@ export { defineApp } from './core/defineApp';
 export type { RuntimeConfig } from './core/defineApp';
 // @umijs/renderer-*
 export { createBrowserHistory, createHashHistory, createMemoryHistory, Helmet, HelmetProvider, createSearchParams, generatePath, matchPath, matchRoutes, Navigate, NavLink, Outlet, resolvePath, useLocation, useMatch, useNavigate, useOutlet, useOutletContext, useParams, useResolvedPath, useRoutes, useSearchParams, useAppData, useClientLoaderData, useRouteProps, useSelectedRoutes, useServerLoaderData, renderClient, __getRoot, Link, useRouteData, __useFetcher, withRouter } from '/workspaces/WebServer/sql-father-frontend-public/node_modules/@umijs/renderer-react';
-export type { History } from '/workspaces/WebServer/sql-father-frontend-public/node_modules/@umijs/renderer-react'
+export type { History } from '/workspaces/WebServer/sql-father-frontend-public/node_modules/@umijs/renderer-react';
 // umi/client/client/plugin
 export { ApplyPluginsType, PluginManager } from '/workspaces/WebServer/sql-father-frontend-public/node_modules/umi/client/client/plugin.js';
 export { history, createHistory } from './core/history';
